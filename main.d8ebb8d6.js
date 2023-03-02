59653f19063b20917da457090110b2a7ae2e929e
Sasha Kolomiiets <kinbean444@gmail.com>
2023-03-02 17:01:26 +0200

    upd build

--- main.d8ebb8d6.js
+++ main.d8ebb8d6.js
@@ -11513,7 +11513,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62085" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64485" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
